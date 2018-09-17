4542653ef8a0dde301b3ffe69c804e75a9ed4ce7
13026118978 <13026118978@163.com>
2018-09-17 18:14:08 +0800

    小调整

--- vue-cells/src/components/index.js
+++ vue-cells/src/components/index.js
@@ -1,12 +1,35 @@
-import VueComment from './test.vue'
-const comment = {
-        install: function(Vue) {
-            Vue.component(VueComment.name, VueComment)
-        }
+/*
+ * @Author: lujing 
+ * @Date: 2018-09-17 17:57:47 
+ * @Last Modified by: lujing
+ * @Last Modified time: 2018-09-17 17:58:14
+ * @Desc:所有的组件都以j开头，区分其他的组件  
+ */
+
+import jTest from './jTest.vue'
+
+
+
+
+// 所有的组件
+const vueCellSViews = {
+        jTest
     }
-    // global 情况下 自动安装
-if (typeof window !== 'undefined' && window.Vue) {
-    window.Vue.use(comment)
+    // 遍历挂载组件
+function install(Vue, _) {
+    Object.keys(vueCellSViews).forEach(key => {
+        Vue.component(key, vueCellSViews[key]);
+    });
 }
+
+// 
+const API = {
+    // version: process.env.VERSION,
+    install
+    // ...vueCellSViews
+}
+
+
 // 导出模块
-export default comment
+export default API
+// module.exports.default = module.exports = API;
